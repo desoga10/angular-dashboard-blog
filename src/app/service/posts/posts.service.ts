03067dd32825ea3b6c405b1055b6e84641ff4906
desoga <done2stay@gmail.com>
2019-06-21 15:13:40 +0100

    Added Post Service

--- src/app/service/posts/posts.service.ts
+++ src/app/service/posts/posts.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { map } from 'rxjs/operators';
+
+export interface Post {
+  title: '';
+  url: '';
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostsService {
+  constructor(private afs: AngularFirestore) {}
 
-  constructor() { }
+  getPosts() {
+    return this.afs
+      .collection('posts')
+      .snapshotChanges()
+      .pipe(
+        map(post => {
+          return post.map(a => {
+            const data = a.payload.doc.data() as Post;
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          });
+        })
+      );
+  }
+
+  addPost(post: Post) {
+    this.afs.collection('posts').add(post);
+  }
+
+  deletePost(postId) {
+    this.afs.doc('posts/' + postId).delete();
+  }
+
+  updatePost(postId, post: Post) {
+    this.afs.doc('post/' + postId).update(post);
+  }
 }
